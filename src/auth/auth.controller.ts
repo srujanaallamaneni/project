fb6c0744fbb336ca46c9d360240e1a74a403a51a
srujanaallamaneni <srujanaa3535@gmail.com>
2025-12-09 05:11:08 +0530

    add Swagger documentation for login endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,13 +2,17 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { Public } from './public.decorator';
-
+import {ApiTags,ApiOperation,ApiResponse} from '@nestjs/swagger'
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
   @Public()
+  @ApiOperation({summary:'User Login'})
+  @ApiResponse({status:201,description:'user logged in successfully'})
+  @ApiResponse({status:401,description:'Invalid credentials.'})
   login(@Body() dto: LoginDto) {
     return this.authService.login(dto);
   }
