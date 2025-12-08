76380f16f9cf4ed20b29e37289797fa47e28b409
srujanaallamaneni <srujanaa3535@gmail.com>
2025-12-09 05:13:48 +0530

    add ApiProperty decorators to CreateSkillDto for improved Swagger documentation

--- src/skills/dto/create-skill.dto.ts
+++ src/skills/dto/create-skill.dto.ts
@@ -1,9 +1,12 @@
 import { IsString } from 'class-validator';
+import {ApiProperty} from '@nestjs/swagger';
 
 export class CreateSkillDto {
+  @ApiProperty({example:'JavaScript',description:'Name of the skill'})
   @IsString()
   name: string;
 
+  @ApiProperty({example:'A programming language',description:'Description of the skill'})
   @IsString()
   description: string;
 }
