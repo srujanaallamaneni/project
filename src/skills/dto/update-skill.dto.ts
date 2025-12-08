74b7367b8277b889001c6aff6ed57475388197f0
srujanaallamaneni <srujanaa3535@gmail.com>
2025-12-09 05:14:42 +0530

    enhance UpdateSkillDto: add ApiPropertyOptional decorators for improved Swagger documentation

--- src/skills/dto/update-skill.dto.ts
+++ src/skills/dto/update-skill.dto.ts
@@ -1,10 +1,13 @@
 import { IsString, IsOptional } from 'class-validator';
+import {ApiPropertyOptional} from '@nestjs/swagger';
 
 export class UpdateSkillDto {
+  @ApiPropertyOptional({example:'JavaScript',description:'Name of the skill'})
   @IsOptional()
   @IsString()
   name?: string;
-
+  
+  @ApiPropertyOptional({example:'A programming language',description:'Description of the skill'})
   @IsOptional()
   @IsString()
   description?: string;
