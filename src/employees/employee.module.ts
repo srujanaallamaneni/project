eb9a520ae3e6e268df7c34c9b8d0567bb247c7de
srujanaallamaneni <srujanaa3535@gmail.com>
2025-12-11 15:47:37 +0530

    enhance EmployeeModule: add Skill schema to Mongoose feature registration

--- src/employees/employee.module.ts
+++ src/employees/employee.module.ts
@@ -7,11 +7,13 @@ import {SkillsModule} from '../skills/skills.module';
 import {MongooseModule} from '@nestjs/mongoose';
 import {Employee,EmployeeSchema} from './employee.schema';
 import { AnalyticsModule }  from '../analytics/analytics.module';
+import { Skill, SkillSchema } from '../skills/skills.schema';
 
 @Module({
   imports: [SkillsModule,
     MongooseModule.forFeature([
-      {name:Employee.name,schema:EmployeeSchema}
+      {name:Employee.name,schema:EmployeeSchema},
+      {name:Skill.name,schema:SkillSchema}
     ])
   ],
   controllers: [EmployeeController],
